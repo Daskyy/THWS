d220045d4133a001c5a6872020e72f8369c4a719
dasky <daskymail@gmail.com>
2024-04-15 15:51:53 +0200

    put questions in psvm

--- prog2/src/main/java/chapter15/quiz/Quiz.java
+++ prog2/src/main/java/chapter15/quiz/Quiz.java
@@ -1,13 +1,12 @@
 package chapter15.quiz;
 
 public class Quiz {
-    static Question q1 = new TextQuestion("Wie ist die Hauptstadt von Deutschland?");
-    static Question q2 = new MultipleChoice("Wie viele Protonen hat ein Wasserstoff-Atom?", new String[]{"4", "2", "1", "0"});
-    static Question q3 = new TextQuestion("Wer ist unser aktueller Bundeskanzler?");
-
-    static Question[] questions = new Question[]{q1, q2, q3};
-
     public static void main(String[] args) {
+        Question q1 = new TextQuestion("Wie ist die Hauptstadt von Deutschland?");
+        Question q2 = new MultipleChoice("Wie viele Protonen hat ein Wasserstoff-Atom?", new String[]{"4", "2", "1", "0"});
+        Question q3 = new TextQuestion("Wer ist unser aktueller Bundeskanzler?");
+
+        Question[] questions = new Question[]{q1, q2, q3};
         for (Question i : questions) {
             i.printQuestion();
         }
